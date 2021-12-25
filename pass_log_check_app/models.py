9dd7b5732f4fc912b9a70fb05c0811ccaf9d9c16
N0ktis <2010zalex@gmail.com>
2021-12-25 03:55:41 +0300

    Add raw field

--- pass_log_check_app/models.py
+++ pass_log_check_app/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 # Create your models here.
 class Passwords(models.Model):
+    raw_password = models.CharField(max_length=256)
     sha256 = models.CharField(max_length=64)
     sha512 = models.CharField(max_length=128)
     md4 = models.CharField(max_length=32)
@@ -12,6 +13,7 @@ class Passwords(models.Model):
 
 
 class Logins(models.Model):
+    raw_login = models.CharField(max_length=256)
     sha256 = models.CharField(max_length=64)
     sha512 = models.CharField(max_length=128)
     md4 = models.CharField(max_length=32)
